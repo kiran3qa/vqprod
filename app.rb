e1837e4ef948d007c07b00b8d0ae3399d14350f9
kiran3qa <76578848+kiran3qa@users.noreply.github.com>
2024-12-16 17:21:45 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,16 +1,11 @@
 require 'sinatra'
 require 'sinatra/json'
-require_relative './controllers/auths_controller'
-
-class App < Sinatra::Base
-  # Mount controllers
-  use AuthsController
 
   # Root route
   get '/' do
     json message: 'Welcome to the Sinatra App!'
   end
-end
+
 
 set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 3000
